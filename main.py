26bf9556f249ee614285904bf13e26be24b9f42a
Cauê Fernandes <cauesantos359@gmail.com>
2026-04-06 22:01:28 -0300

    Erro proposital

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ app = FastAPI()
 def read_root():
     return {"mensagem": "API funcionando!"}
 
-@app.get("/somar/{a}/{b}")
+@app.get("/somar/{a}/{b}") 
 def somar(a: int, b: int):
-    # Apenas retorna a soma dos dois números
-    return {"resultado": a + b}
+    return {"resultado": a - b}  # errado de propósito
